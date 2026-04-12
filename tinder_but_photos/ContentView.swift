29b34e2b3a91e07ffd6bd778a17c1d25d5546257
Andrés <andres.onate.e@ug.uchile.cl>
2026-04-11 20:11:55 -0400

    Initial Commit

---
+++ tinder_but_photos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tinder_but_photos
+//
+//  Created by Andrés Oñate Escobedo on 11-04-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tinder_but_photos/tinder_but_photosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinder_but_photosApp.swift
+//  tinder_but_photos
+//
+//  Created by Andrés Oñate Escobedo on 11-04-26.
+//
+
+import SwiftUI
+
+@main
+struct tinder_but_photosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
